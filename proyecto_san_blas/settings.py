8acab2c3e4b53915df4154214e682bce4a48f11a
Jose Contreras Stoltze <jmcontreras.stoltze@gmail.com>
2024-07-24 01:51:49 -0400

    breadcrumbs incompleto

--- proyecto_san_blas/settings.py
+++ proyecto_san_blas/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'san_blas_app.context_processors.breadcrumbs_processor' ##########################################
             ],
         },
     },
---
+++ san_blas_app/templatetags/breadcrumb_tags.py
@@ -0,0 +1,128 @@
+from django import template
+from django.urls import resolve
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def breadcrumbs(context):
+    request = context['request']
+    url_name = resolve(request.path_info).url_name
+    # path = request.path_info
+    breadcrumbs = []
+
+    if url_name == 'contacto':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Contacto', 'url': None}
+        ]
+    elif url_name == 'newsletter':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Newsletter', 'url': None}            
+        ]
+    elif url_name == 'registro':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Registro', 'url': None}            
+        ]
+    elif url_name == 'dashboard':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': None}
+        ]
+    elif url_name == 'perfil_usuario':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Perfil Usuario', 'url': None}
+        ]
+    elif url_name == 'editar_usuario':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Perfil Usuario', 'url': '/home/dasboard/mi-perfil'},
+            {'title': 'Editar Perfil', 'url': None}
+        ]
+    elif url_name == 'mascotas':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Mascotas', 'url': None}
+        ]
+    elif url_name == 'registro_mascota':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Mascotas', 'url': '/home/dashboard/mis-mascotas/'},
+            {'title': 'Registro Mascota', 'url': None},
+        ]
+    elif url_name == 'reservar_hora':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Reservar Cita', 'url': None}
+        ]
+    elif url_name == 'agendamientos':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Agendamientos', 'url': None}
+        ]
+    elif url_name == 'resenas_usuarios':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Comentarios', 'url': None}
+        ]
+    elif url_name == 'vacunatorio':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Vacunatorio', 'url': None}
+        ]
+    elif url_name == 'vacunas_mascota':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Historial Vacunas', 'url': None}
+        ]
+    elif url_name == 'dashboard_admin':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': None}
+        ]
+    elif url_name == 'clientes':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': '/home/dashboard-admin/'},
+            {'title': 'Clientes', 'url': None}
+        ]
+    elif url_name == 'pacientes':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': '/home/dashboard-admin/'},
+            {'title': 'Pacientes', 'url': None}
+        ]
+    elif url_name == 'agenda':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': '/home/dashboard-admin/'},            
+            {'title': 'Agenda', 'url': None},
+        ]
+    elif url_name == 'consulta':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Agenda', 'url': '/home/dashboard-admin/agenda'},
+            {'title': 'Consulta', 'url': None},
+        ]
+    elif url_name == 'registro_vacuna':
+        breadcrumbs = [
+            {'title': 'Inicio', 'url': '/home'},
+            {'title': 'Dashboard', 'url': '/home/dashboard-admin/'},
+            {'title': 'Agenda', 'url': '/home/dashboard-admin/agenda'},
+            {'title': 'Consulta', 'url': '/home/dashboard-admin/consulta'},
+            {'title': 'Registro Vacuna', 'url': None},
+        ]    
+
+    return breadcrumbs
