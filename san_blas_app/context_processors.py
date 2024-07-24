a2ffc70d39d8a0eb7bb6b38270634e115c720ae5
Jose Contreras Stoltze <jmcontreras.stoltze@gmail.com>
2024-07-24 10:59:07 -0400

    breadcrumbs, detalles front notificaciones

--- san_blas_app/context_processors.py
+++ san_blas_app/context_processors.py
@@ -29,57 +29,57 @@ def breadcrumbs_processor(request):
     elif url_name == 'perfil_usuario':
         breadcrumbs = [
             {'title': 'Inicio', 'url': '/home'},
-            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Dashboard', 'url': '/home/dashboard'},
             {'title': 'Perfil Usuario', 'url': None}
         ]
     elif url_name == 'editar_usuario':
         breadcrumbs = [
             {'title': 'Inicio', 'url': '/home'},
-            {'title': 'Dashboard', 'url': '/home/dashboard/'},
-            {'title': 'Perfil Usuario', 'url': '/home/dasboard/mi-perfil'},
+            {'title': 'Dashboard', 'url': '/home/dashboard'},
+            {'title': 'Perfil Usuario', 'url': '/home/dashboard/mi-perfil'},
             {'title': 'Editar Perfil', 'url': None}
         ]
     elif url_name == 'mascotas':
         breadcrumbs = [
             {'title': 'Inicio', 'url': '/home'},
-            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Dashboard', 'url': '/home/dashboard'},
             {'title': 'Mascotas', 'url': None}
         ]
     elif url_name == 'registro_mascota':
         breadcrumbs = [
             {'title': 'Inicio', 'url': '/home'},
-            {'title': 'Dashboard', 'url': '/home/dashboard/'},
-            {'title': 'Mascotas', 'url': '/home/dashboard/mis-mascotas/'},
+            {'title': 'Dashboard', 'url': '/home/dashboard'},
+            {'title': 'Mascotas', 'url': '/home/dashboard/mis-mascotas'},
             {'title': 'Registro Mascota', 'url': None},
         ]
     elif url_name == 'reservar_hora':
         breadcrumbs = [
             {'title': 'Inicio', 'url': '/home'},
-            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Dashboard', 'url': '/home/dashboard'},
             {'title': 'Reservar Cita', 'url': None}
         ]
     elif url_name == 'agendamientos':
         breadcrumbs = [
             {'title': 'Inicio', 'url': '/home'},
-            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Dashboard', 'url': '/home/dashboard'},
             {'title': 'Agendamientos', 'url': None}
         ]
     elif url_name == 'resenas_usuarios':
         breadcrumbs = [
             {'title': 'Inicio', 'url': '/home'},
-            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Dashboard', 'url': '/home/dashboard'},
             {'title': 'Comentarios', 'url': None}
         ]
     elif url_name == 'vacunatorio':
         breadcrumbs = [
             {'title': 'Inicio', 'url': '/home'},
-            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Dashboard', 'url': '/home/dashboard'},
             {'title': 'Vacunatorio', 'url': None}
         ]
     elif url_name == 'vacunas_mascota':
         breadcrumbs = [
             {'title': 'Inicio', 'url': '/home'},
-            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Dashboard', 'url': '/home/dashboard'},
             {'title': 'Historial Vacunas', 'url': None}
         ]
     elif url_name == 'dashboard_admin':
@@ -90,32 +90,32 @@ def breadcrumbs_processor(request):
     elif url_name == 'clientes':
         breadcrumbs = [
             {'title': 'Inicio', 'url': '/home'},
-            {'title': 'Dashboard', 'url': '/home/dashboard-admin/'},
+            {'title': 'Dashboard', 'url': '/home/dashboard-admin'},
             {'title': 'Clientes', 'url': None}
         ]
     elif url_name == 'pacientes':
         breadcrumbs = [
             {'title': 'Inicio', 'url': '/home'},
-            {'title': 'Dashboard', 'url': '/home/dashboard-admin/'},
+            {'title': 'Dashboard', 'url': '/home/dashboard-admin'},
             {'title': 'Pacientes', 'url': None}
         ]
     elif url_name == 'agenda':
         breadcrumbs = [
             {'title': 'Inicio', 'url': '/home'},
-            {'title': 'Dashboard', 'url': '/home/dashboard-admin/'},            
+            {'title': 'Dashboard', 'url': '/home/dashboard-admin'},            
             {'title': 'Agenda', 'url': None},
         ]
     elif url_name == 'consulta':
         breadcrumbs = [
             {'title': 'Inicio', 'url': '/home'},
-            {'title': 'Dashboard', 'url': '/home/dashboard/'},
+            {'title': 'Dashboard', 'url': '/home/dashboard'},
             {'title': 'Agenda', 'url': '/home/dashboard-admin/agenda'},
             {'title': 'Consulta', 'url': None},
         ]
     elif url_name == 'registro_vacuna':
         breadcrumbs = [
             {'title': 'Inicio', 'url': '/home'},
-            {'title': 'Dashboard', 'url': '/home/dashboard-admin/'},
+            {'title': 'Dashboard', 'url': '/home/dashboard-admin'},
             {'title': 'Agenda', 'url': '/home/dashboard-admin/agenda'},
             {'title': 'Consulta', 'url': '/home/dashboard-admin/consulta'},
             {'title': 'Registro Vacuna', 'url': None},
